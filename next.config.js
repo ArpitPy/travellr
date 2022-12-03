f112af0c4768954796e045808b1a814738c3f43a
Arpit <arpitpandeyofc@gmail.com>
2022-12-03 14:33:07 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  images: {
+    domains: ['i.postimg.cc', 'links.papareact.com', 'jsonkeeper.com'],
+  }
 }
-
-module.exports = nextConfig
